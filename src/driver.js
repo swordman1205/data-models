a60e29ff2f2dce0cda9164d121d5e5816670b551
Bridget Almas <balmsa@gmail.com>
2017-11-09 07:44:46 -0500

    export Constants

--- src/driver.js
+++ src/driver.js
@@ -1,4 +1,5 @@
 'use strict'
+import * as Constants from './constants.js'
 import SourceSelection from './source_selection.js'
 import Feature from './feature.js'
 import FeatureType from './feature_type.js'
@@ -11,4 +12,4 @@ import Lemma from './lemma.js'
 import Inflection from './inflection.js'
 import LatinLanguageModel from './latin_language_model.js'
 import GreekLanguageModel from './greek_language_model.js'
-export { SourceSelection, Feature, FeatureType, FeatureList, FeatureImporter, Inflection, LanguageModelFactory, Homonym, Lexeme, Lemma, LatinLanguageModel, GreekLanguageModel }
+export { Constants, SourceSelection, Feature, FeatureType, FeatureList, FeatureImporter, Inflection, LanguageModelFactory, Homonym, Lexeme, Lemma, LatinLanguageModel, GreekLanguageModel }
