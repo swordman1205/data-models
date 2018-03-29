38eb6aed9e8d424a4928b9cbf87f5725507d7911
Kirlat <kirlat@yula.media>
2018-03-29 15:32:32 +0400

    A fix for alpheios-project/inflection-tables#26

--- dist/alpheios-data-models.js
+++ dist/alpheios-data-models.js
@@ -1899,7 +1899,9 @@ class GreekLanguageModel extends LanguageModel {
     );
     for (const matchingForm of matchingForms) {
       if (matchingForm.features.hasOwnProperty(Feature.types.grmClass)) {
-        matchingValues.add(matchingForm.features[Feature.types.grmClass]);
+        for (const value of matchingForm.features[Feature.types.grmClass].values) {
+          matchingValues.add(value);
+        }
       }
     }
     if (matchingValues.size > 0) {
@@ -2239,9 +2241,10 @@ class Feature {
       // Single value with no sort order
       normalized = [[data, this.defaultSortOrder]];
     } else if (!Array.isArray(data[0])) {
-      // Multiple values without sort order
+      // Multiple values without any sort order, default sort order will be used
       normalized = data.map((v, i) => [v, i + 1]);
     } else {
+      // Value has all the data, including a sort order
       normalized = data;
     }
     return normalized.map(d => { return { value: d[0], sortOrder: Number.parseInt(d[1]) } })
@@ -2584,7 +2587,12 @@ class Feature {
     if (!Array.isArray(foreignData)) {
       foreignData = [foreignData];
     }
-    const values = foreignData.map(fv => importer.get(fv));
+    let values = foreignData.map(fv => importer.get(fv));
+    /*
+    Some values may be mapped into multiple values. For them an importer will return an array of values instead of a single value.
+    The values will be a multidimensional array that will require flattening.
+     */
+    values = values.reduce((acc, cv) => acc.concat(cv), []);
     return new Feature(this.type, values, this.languageID, this.sortOrder, this.allowedValues)
   }
 }
